50883cd6f3c571ed3faa9823e8648c88fa01fc4b
coffee-kunren <monkeymasa0405@icloud.com>
2025-08-25 15:29:28 +0900

    サービスを作成

---
+++ src/main/java/com/example/demo/service/impl/TodoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TodoServiceImpl {
+
+}
