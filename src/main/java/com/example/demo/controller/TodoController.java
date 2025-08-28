63bd2bded5e29bc611ad4f36ca86542ca86787ac
coffee-kunren <monkeymasa0405@icloud.com>
2025-08-28 12:22:11 +0900

    「@Valid」を「@Validated」に変更

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,6 @@ import com.example.demo.form.TodoForm;
 import com.example.demo.helper.TodoHelper;
 import com.example.demo.service.TodoService;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -51,7 +51,7 @@ public class TodoController {
 
 	//TODO新規作成処理
 	@PostMapping("save")
-	public String save(@Valid TodoForm todoForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+	public String save(@Validated TodoForm todoForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 		if (bindingResult.hasErrors()) {
 			return "form";//入力エラーなら処理せず返す
 		}
